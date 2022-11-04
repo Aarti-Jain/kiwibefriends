f1e7ba57ed9c9b919b3ab4183a9e7f2075bec507
Aarti-Jain <aartijain@brandeis.edu>
2022-11-04 19:06:02 -0400

    chapter 10 functionality added

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2022_10_29_215535) do
+ActiveRecord::Schema[7.0].define(version: 2022_11_04_225027) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -20,6 +20,8 @@ ActiveRecord::Schema[7.0].define(version: 2022_10_29_215535) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.string "password_digest"
+    t.string "remember_digest"
+    t.boolean "admin", default: false
   end
 
 end
