eef36ef277fe894b34b7968897efb98e5cd4448e
MahmoudSalah02 <mahmouds343@gmail.com>
2022-11-22 16:17:19 -0500

    search functionality for users (not responsive)

--- test/integration/users_index_test.rb
+++ test/integration/users_index_test.rb
@@ -1,11 +1,9 @@
 require "test_helper"
 
 class UsersIndexTest < ActionDispatch::IntegrationTest
-
   def setup
     @admin     = users(:michael)
     @non_admin = users(:archer)
-
   end
 
   test "index as admin including pagination and delete links" do
@@ -27,11 +25,28 @@ class UsersIndexTest < ActionDispatch::IntegrationTest
     end
   end
 
-
   test "index as non-admin" do
     log_in_as(@non_admin)
     get users_path
     assert_select 'a', text: 'delete', count: 0
   end
-  
+
+  test "index search bar no query" do
+    log_in_as(@non_admin)
+    get users_path
+    assert_template 'users/index'
+    assert_select 'div.pagination'
+    first_page_of_users = User.paginate(page: 1)
+    first_page_of_users.each do |user|
+      assert_select 'a[href=?]', user_path(user), text: user.name
+    end
+  end
+
+  test "index search bar" do
+    log_in_as(@non_admin)
+    get users_path, params: { query: "us" }
+    assert_template 'users/index'
+
+    # TODO
+  end
 end
