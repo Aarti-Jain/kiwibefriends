cb87722b327e1ecb5d822ec90465ab528bd9ec5c
MahmoudSalah02 <mahmouds343@gmail.com>
2022-11-24 15:06:54 -0500

    existing users are redirected to the home feed after logging in

--- test/integration/users_login_test.rb
+++ test/integration/users_login_test.rb
@@ -4,7 +4,7 @@ class UsersLoginTest < ActionDispatch::IntegrationTest
   def setup
     @user = users(:michael)
   end
-  
+
   test "login with valid email/invalid password" do
     get login_path
     assert_template 'sessions/new'
@@ -22,9 +22,9 @@ class UsersLoginTest < ActionDispatch::IntegrationTest
     post login_path, params: { session: { email:    @user.email,
                                           password: 'password' } }
     assert is_logged_in?
-    assert_redirected_to @user
+    assert_redirected_to root_path
     follow_redirect!
-    assert_template 'users/show'
+    assert_template 'static_pages/home'
     assert_select "a[href=?]", login_path, count: 0
     assert_select "a[href=?]", logout_path
     assert_select "a[href=?]", user_path(@user)
