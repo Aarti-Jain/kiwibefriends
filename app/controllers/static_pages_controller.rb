d0887571e8754f8f87bac6d9461cb321dd8db9c5
MahmoudSalah02 <mahmouds343@gmail.com>
2022-11-24 15:31:53 -0500

    users who have no following now see popular posts on their feed

--- app/controllers/static_pages_controller.rb
+++ app/controllers/static_pages_controller.rb
@@ -4,6 +4,11 @@ class StaticPagesController < ApplicationController
     if logged_in?
       @micropost  = current_user.microposts.build
       @feed_items = current_user.feed.paginate(page: params[:page])
+      @no_posts = false
+      if @feed_items.count.zero?
+        @no_posts = true
+        @feed_items = Micropost.where('rating >= 4').paginate(page: params[:page])
+      end
     else
       redirect_to(login_url)
     end
