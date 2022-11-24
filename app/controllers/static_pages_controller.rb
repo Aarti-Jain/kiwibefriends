be3db19ea391538e1a902cc54b43287da75ff921
MahmoudSalah02 <mahmouds343@gmail.com>
2022-11-24 14:56:35 -0500

    home page redirects user to login if not logged in

--- app/controllers/static_pages_controller.rb
+++ app/controllers/static_pages_controller.rb
@@ -4,6 +4,8 @@ class StaticPagesController < ApplicationController
     if logged_in?
       @micropost  = current_user.microposts.build
       @feed_items = current_user.feed.paginate(page: params[:page])
+    else
+      redirect_to(login_url)
     end
   end
 
