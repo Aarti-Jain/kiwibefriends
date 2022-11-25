776cf0acb480505197076c7b895fd9ae28f9c977
MahmoudSalah02 <mahmouds343@gmail.com>
2022-11-25 11:53:37 -0500

    update profile picture for user

--- app/helpers/users_helper.rb
+++ app/helpers/users_helper.rb
@@ -1,10 +1,13 @@
 module UsersHelper
-
   # Returns the Gravatar for the given user.
   def gravatar_for(user, options = { size: 80 })
-    size         = options[:size]
-    gravatar_id  = Digest::MD5::hexdigest(user.email.downcase)
-    gravatar_url = "https://secure.gravatar.com/avatar/#{gravatar_id}?s=#{size}"
-    image_tag(gravatar_url, alt: user.name, class: "gravatar")
+    size = options[:size]
+    if user.image.attached?
+      image_tag(user.image, alt: user.name, class: "gravatar", size: size)
+    else
+      gravatar_id  = Digest::MD5::hexdigest(user.email.downcase)
+      gravatar_url = "https://secure.gravatar.com/avatar/#{gravatar_id}?s=#{size}"
+      image_tag(gravatar_url, alt: user.name, class: "gravatar")
+    end
   end
 end
