97cb64f3a16992010a7f61eb1f54dc4b4a61acea
MahmoudSalah02 <mahmouds343@gmail.com>
2022-11-25 12:52:31 -0500

    reduced the size of the seed data

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,41 +1,49 @@
 require 'faker'
 
-# Create a main sample user.
-User.create!(name:  "Example User",
-    email: "example@railstutorial.org",
-    password:              "foobar",
-    password_confirmation: "foobar",
-    admin: true)
+# creating an admin user
+User.create!(name:  "admin",
+             email: "admin@gmail.com",
+             password: "admin123",
+             password_confirmation: "admin123",
+             admin: true)
 
-# Generate a bunch of additional users.
-99.times do |n|
-    name  = Faker::Name.name
-    email = "example-#{n+1}@railstutorial.org"
-    password = "password"
-    User.create!(name:  name,
-                 email: email,
-                 password:              password,
-                 password_confirmation: password)
+# Generate a bunch of additional Users.
+30.times do |n|
+  name  = Faker::Name.name
+  email = "user-#{n+1}@gmail.com"
+  password = "password"
+  new_user = User.create!(name:  name,
+                          email: email,
+                          password: password,
+                          password_confirmation: password)
+  # new_user.image.attach(
+  #   io:  File.open(File.join(Rails.root,'app/assets/images/photo.jpg')),
+  #   filename: 'photo.jpg'
+  # )
 end
 
 # Generate a bunch of Restaurants
-99.times do
+30.times do
   name = Faker::Restaurant.unique.name
   description = Faker::Restaurant.description
-  Restaurant.create!(name: name,
-                     description: description[0...100])
+  new_restaurant = Restaurant.create!(name: name,
+                                      description: description[0...220])
+  # new_restaurant.image.attach(
+  #   io:  File.open(File.join(Rails.root,'app/assets/images/photo.jpg')),
+  #   filename: 'photo.jpg'
+  # )
 end
 
 # Generate microposts
 users = User.all
 users.each do |user|
-  restaurants_subset = Restaurant.order('RANDOM()').take(5)
+  restaurants_subset = Restaurant.order('RANDOM()').take(3)
   restaurants_subset.each do |restaurant|
-    content = Faker::Lorem.sentence(word_count: Faker::Number.between(from: 10, to: 20))[0...200]
+    content = Faker::Restaurant.review[0...200]
     rating = Faker::Number.between(from: 1, to: 5)
     restaurant_id = restaurant.id
-    user.microposts.create!(content: content, 
-                            restaurant_id:restaurant_id,
+    user.microposts.create!(content: content,
+                            restaurant_id: restaurant_id,
                             rating: rating)
   end
 end
@@ -51,15 +59,15 @@ followers.each { |follower| follower.follow(user) }
 # Create restaurant following relationships.
 users = User.all
 users.each do |curr_user|
-  restaurants_to_follow = Restaurant.order('RANDOM()').take(5)
+  restaurants_to_follow = Restaurant.order('RANDOM()').take(3)
   restaurants_to_follow.each do |curr_restaurant|
     curr_user.restaurant_follow(curr_restaurant)
   end
 end
 
-# Create restaurant following relationships.
+# Create micrpost liking relationships.
 Micropost.all.each do |micropost|
-  User.order('RANDOM()').take(Faker::Number.between(from: 2, to: 50)).each do |curr_user|
+  User.order('RANDOM()').take(Faker::Number.between(from: 2, to: 20)).each do |curr_user|
     curr_user.like(micropost)
   end
 end
