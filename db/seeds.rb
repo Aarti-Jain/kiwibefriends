bd6db4616b74082d5c5c65b26a6f3dedac5becb7
MahmoudSalah02 <mahmouds343@gmail.com>
2022-12-10 15:01:19 -0500

    use google places api to get a list of 20 restaurants in MA and store them in restaurants table

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,4 +1,6 @@
 require 'faker'
+require "uri"
+require "net/http"
 
 # creating an admin user
 User.create!(name:  "admin",
@@ -7,74 +9,21 @@ User.create!(name:  "admin",
              password_confirmation: "admin123",
              admin: true)
 
-# Generate a bunch of additional Users.
-(1..20).each do |i|
-  name  = Faker::Name.name
-  email = "user-#{i+1}@gmail.com"
-  password = "password"
-  new_user = User.create!(name:  name,
-                          email: email,
-                          password: password,
-                          password_confirmation: password)
-  new_user.image.attach(
-    io:  File.open(File.join(Rails.root,"app/assets/images/users/#{i}.jpg")),
-    filename: "#{i}.jpg"
-  )
-end
+# Use Google Places API - Text Search to get a list of 20 Restaurants
+url = URI("https://maps.googleapis.com/maps/api/place/textsearch/json?query=restaurants%20in%20Massachusetts&key=#{Rails.application.credentials.google_maps_api_key}")
 
-# Generate a bunch of Restaurants
-(1..26).each do |i|
-  name = Faker::Restaurant.unique.name
-  full_address = Faker::Address.full_address
-  description = full_address[full_address.index(',') + 2..]
-  new_restaurant = Restaurant.create!(name: name,
-                                      description: description[0...220])
-  new_restaurant.image.attach(
-    io:  File.open(File.join(Rails.root,"app/assets/images/restaurants/#{i}.jpg")),
-    filename: "#{i}.jpg"
-  )
-end
+https = Net::HTTP.new(url.host, url.port)
+https.use_ssl = true
 
-# Generate microposts
-users = User.all
-i = 1
-users.each do |user|
-  restaurants_subset = Restaurant.order('RANDOM()').take(3)
-  restaurants_subset.each do |restaurant|
-    content = Faker::Restaurant.review[0...200]
-    rating = Faker::Number.between(from: 1, to: 5)
-    restaurant_id = restaurant.id
-    new_micropost = user.microposts.create!(content: content,
-                                            restaurant_id: restaurant_id,
-                                            rating: rating)
-    new_micropost.image.attach(
-      io:  File.open(File.join(Rails.root,"app/assets/images/food/#{i % 28 + 1}.jpg")),
-      filename: "#{i % 28}.jpg"
-    )
-    i += 1
-  end
-end
+request = Net::HTTP::Get.new(url)
 
-# Create following relationships.
-users = User.all
-user  = users.first
-following = users[2..50]
-followers = users[3..40]
-following.each { |followed| user.follow(followed) }
-followers.each { |follower| follower.follow(user) }
+response = https.request(request)
+restaurants_data = JSON.parse(response.read_body)
 
-# Create restaurant following relationships.
-users = User.all
-users.each do |curr_user|
-  restaurants_to_follow = Restaurant.order('RANDOM()').take(3)
-  restaurants_to_follow.each do |curr_restaurant|
-    curr_user.restaurant_follow(curr_restaurant)
-  end
-end
-
-# Create micrpost liking relationships.
-Micropost.all.each do |micropost|
-  User.order('RANDOM()').take(Faker::Number.between(from: 2, to: 20)).each do |curr_user|
-    curr_user.like(micropost)
-  end
+# Store that data in the restaurants table
+restaurants_data["results"].each do |restaurant|
+  new_restaurant = Restaurant.create!(name: restaurant["name"], 
+                                      description: restaurant["formatted_address"].split(",")[1..2].join(",").strip!,
+                                      latitude: restaurant["geometry"]["location"]["lat"],
+                                      longitude: restaurant["geometry"]["location"]["lng"])
 end
